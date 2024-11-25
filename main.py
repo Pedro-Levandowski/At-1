e223e23c2beaf68b4f49c1ced12a4dd66d883dea
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2024-11-25 19:46:45 -0300

    Cria a função de soma
    Criação da função de soma que retorna uma string que informa a soma entre 2 números

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def imprimir_mensagem(texto):
     print(texto)
 
+def somar(a,b):
+    return f'A soma de {a} com {b} é: {int(a) + int(b)}'
+
 print('Hello, World')
 
 imprimir_mensagem('Seja muito bem-Vindo(a)!')
+
+print(somar(10,5))
